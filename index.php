8db076b8e87d6396df833b1dcade40cf88f58f73
Bas van Manen <basje1@gmail.com>
2013-06-19 23:25:18 +0200

    Added example routing

--- index.php
+++ index.php
@@ -9,6 +9,47 @@ define('BASE', realpath('.').DIRECTORY_SEPARATOR);
 $uri = trim(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PHP_SELF']), '/');
 $segments = strpos($uri, '/') !== false
                 ? explode('/', $uri)
-                : array(0=>$uri);
+                : array(
+                    0 => $uri
+                );
 
-print_r($segments);
+
+/**
+ * Example code
+ */
+$route = function ($segments) {
+
+    /**
+     * Here is it about all you wanna do, basically this closure gets executed
+     * with the segment array as parameter. Here you can perform checks and
+     * build your own custom routing code.
+     *
+     * For example you could make sure the first param is always the name of the
+     * file without the php extension or you could match the first 2 params with
+     * folders/filename and you could send through the rest of the segments for
+     * params used in the request for instance: folder1/file1/id/55
+     */
+
+    /**
+     * example route
+     */
+
+    /**
+     * Only 1 segment? load php file
+     */
+    if (count($segments) == 1) {
+        require_once BASE . $segments[0].'.php';
+    }
+
+    /**
+     * More than one segment? indent into folder then load the file
+     * the rest of the segments are parameters for the page
+     */
+    if (count($segments) > 1) {
+        if (is_dir($segments[0])) {
+            require_once $segments[0].'/'.$segments[1].'.php';
+        }
+    }
+};
+
+$route($segments);
